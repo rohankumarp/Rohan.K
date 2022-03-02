ebea314d42b346a4aa1619a995546e79bc6dba70
rohankumarp <rohankumarraj@gmail.com>
2022-03-02 13:18:24 +0530

    Files deleted

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -9,7 +9,7 @@ export default function portfolio() {
         </div>
         <div className="body">
           <span>
-          I'm am a passionate Web Developer Developer with good background in Machine Learning and Data Science. I also have knowledge of different tech stacks such as  .I am a self-motivated, hard-working team player who loves to make things that make a difference in the world. 
+          I'm am a passionate Web Developer Developer with good background in Machine Learning and Data Science. I also have knowledge of different tech stacks such as  .I am a self-motivated, hard-working team player who loves to implement new skills into learn world projects. 
           </span>
         </div>
       </div>
