838b6812f9c25d7072f18b865c55d4f228f8521f
rohankumarp <rohankumarraj@gmail.com>
2022-03-02 20:02:47 +0530

    portfolio updated

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,12 +4,12 @@ export default function portfolio() {
   return (
     <div className='portfolio' id='portfolio'>
       <div className='wrapper'>
-        <div className="Headding">
+        <div className="heading">
           About Me.....
         </div>
         <div className="body">
           <span>
-          I'm am a passionate Web Developer Developer with good background in Machine Learning and Data Science. I also have knowledge of different tech stacks such as  .I am a self-motivated, hard-working team player who loves to make things that make a difference in the world. 
+          I'm am a passionate Web Developer Developer with good background in Machine Learning and Data Science. I am a self-motivated, hard-working team player who loves to make things that make a difference in the world. 
           </span>
         </div>
       </div>
