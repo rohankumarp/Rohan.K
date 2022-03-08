5eb5410a4945e3301e95ec59da60f6d56346fd78
rohankumarp <rohankumarraj@gmail.com>
2022-03-08 14:12:17 +0530

    all done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,11 @@
-import { useState } from "react";
+import React, { useRef } from 'react';
+import emailjs from "@emailjs/browser";
 import "./contact.scss"
+import { useState } from "react";
+import{ init } from '@emailjs/browser';
+init("53KBQUoBO1fZr0hpE");
+
+
 
 export default function Contact() {
   const [message,setMessage]=useState(false)
@@ -7,6 +13,20 @@ export default function Contact() {
   const handleSubmit=(e)=>{
     e.preventDefault();
     setMessage(true)
+  }
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_q4t2oln', 'template_xs2zbb6', e.target, '53KBQUoBO1fZr0hpE')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+    });
+
+    e.target.reset()
+
   }
   return (
     <div className='contact' id='contact'>
@@ -16,10 +36,10 @@ export default function Contact() {
 
       <div className="right">
         <p>Lets Talk !</p>
-        <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Name"></input>
-          <input type="text" placeholder="Email"></input>
-          <textarea placeholder="Enter Message"></textarea>
+        <form  onSubmit={sendEmail}>
+          <input type="text" placeholder="Name" name="name"></input>
+          <input type="email" placeholder="Email" name="email"></input>
+          <textarea placeholder="Enter Message" name="message"></textarea>
           <div class="fancybutton">
             <button type="submit"><span>Send</span></button>
             {message && <span className="replytext">Reply on the way :)</span>}
